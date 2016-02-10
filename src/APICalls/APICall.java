fa65addbb6dad376d047e668166ed433cd60c34f
markteo <mark-543210@hotmail.com>
2016-02-10 16:53:40 +0800

    added more methods for api calls

--- src/APICalls/APICall.java
+++ src/APICalls/APICall.java
@@ -11,7 +11,8 @@ import java.net.URLEncoder;
 import main.Helper;
 
 public class APICall {
-	public static String loginBucket(String targetURL){
+
+	public static String loginBucket(String targetURL) {
 		String api = "login";
 		targetURL = targetURL + api;
 		String urlParameters;
@@ -19,23 +20,73 @@ public class APICall {
 
 			String username = Helper.readString("Enter username > ");
 			String password = Helper.readString("Enter password > ");
-			urlParameters = "user-name="
-					+ URLEncoder.encode(username, "UTF-8") + "&password="
-					+ URLEncoder.encode(password, "UTF-8");
-			urlParameters = "user-name=" + URLEncoder.encode("root", "UTF-8") + "&password="
-					+ URLEncoder.encode("root", "UTF-8");
+			urlParameters = "user-name=" + URLEncoder.encode(username, "UTF-8")
+					+ "&password=" + URLEncoder.encode(password, "UTF-8");
 
 			String response = executePost(targetURL, urlParameters);
 			System.out.println(response);
 			return response;
 		} catch (UnsupportedEncodingException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
 		return null;
 
+	}
 
+	public static String getInventoryItem(String targetURL, String sessionKey) {
+
+		String api = "getinventorylist";
+		targetURL = targetURL + api;
+		String urlParameters;
+		try {
+			urlParameters = "session-key="
+					+ URLEncoder.encode(sessionKey, "UTF-8");
+			String response = executePost(targetURL, urlParameters);
+			System.out.println(response);
+			return response;
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+
+		return null;
+	}
+	
+	public static String updateInventoryList(String targetURL, String sessionKey){
+		
+		String api = "updateinventory";
+		targetURL = targetURL + api;
+		String urlParameters;
+		
+		try {
+			urlParameters = "session-key="
+					+ URLEncoder.encode(sessionKey, "UTF-8");
+			String response = executePost(targetURL, urlParameters);
+			System.out.println(response);
+			return response;
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+		
+		
+		return null;
+	}
+	
+	public static String getBucketDevices(String targetURL, String sessionKey){
+		
+		String api = "getuserdevices";
+		targetURL = targetURL + api;
+		String urlParameters;
+		try {
+			urlParameters = "session-key="
+					+ URLEncoder.encode(sessionKey, "UTF-8");
+			String response = executePost(targetURL, urlParameters);
+			System.out.println(response);
+			return response;
+		} catch (UnsupportedEncodingException e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
 
 	public static String executePost(String targetURL, String urlParameters) {
@@ -46,9 +97,11 @@ public class APICall {
 			url = new URL(targetURL);
 			connection = (HttpURLConnection) url.openConnection();
 			connection.setRequestMethod("POST");
-			connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+			connection.setRequestProperty("Content-Type",
+					"application/x-www-form-urlencoded");
 
-			connection.setRequestProperty("Content-Length", "" + Integer.toString(urlParameters.getBytes().length));
+			connection.setRequestProperty("Content-Length",
+					"" + Integer.toString(urlParameters.getBytes().length));
 			connection.setRequestProperty("Content-Language", "en-US");
 
 			connection.setUseCaches(false);
@@ -56,7 +109,8 @@ public class APICall {
 			connection.setDoOutput(true);
 
 			// Send request
-			DataOutputStream wr = new DataOutputStream(connection.getOutputStream());
+			DataOutputStream wr = new DataOutputStream(
+					connection.getOutputStream());
 			wr.writeBytes(urlParameters);
 			wr.flush();
 			wr.close();
@@ -86,4 +140,3 @@ public class APICall {
 		}
 	}
 }
-
