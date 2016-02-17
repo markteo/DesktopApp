ecd0d4d62f55c1938e014a12c02cf9e4c28f31bf
markteo <mark-543210@hotmail.com>
2016-02-17 11:17:07 +0800

    integrate UIInventorySelect

--- src/ui/UIInventorySelect.java
+++ src/ui/UIInventorySelect.java
@@ -17,13 +17,23 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.ListSelectionModel;
 
-import customColor.CustomColor;
+import main.Data;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import ui.components.Button;
 import ui.components.Label;
 import ui.components.Layouts;
 import ui.components.Panel;
+import api.APICall;
+import api.APIProcess;
+import customColor.CustomColor;
 
 public class UIInventorySelect {
+	public static APICall api = new APICall();
+
 	public void runInventorySelect() {
 		Panel p = new Panel();
 		Button b = new Button();
@@ -31,10 +41,21 @@ public class UIInventorySelect {
 
 		DefaultListModel<String> model = new DefaultListModel<String>();
 		// fetch list from server
-		for (int i = 0; i < 10; i++){
-			model.addElement("Element " + i);
-		}
+		model.addElement("ID  " + " | " + "      Registation Number       |     MAC Address");
+		JSONArray inventoryList = new APIProcess().inventoryList(Data.targetURL, Data.sessionKey);
 
+		try {
+			System.out.println(inventoryList.length());
+			for (int i = 0; i < inventoryList.length(); i++){
+				
+				JSONObject inventoryItem = inventoryList.getJSONObject(i);
+				model.addElement(inventoryItem.get("id") + "  |    " + inventoryItem.get("registrationNumber") + "      |    " + inventoryItem.get("macAddress"));
+				
+			}
+		} catch (JSONException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
 		// start of ui
 		JFrame inventoryFrame = new JFrame("Inventory");
 		inventoryFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
@@ -66,6 +87,7 @@ public class UIInventorySelect {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				// add inventory code here
+				// open add frame and close current frame.
 				model.addElement("Element Test");
 			}
 		});
