0578c0994903a6c97be85bc0722e61a91be0586f
jccheok <cheok.jia.chin@gmail.com>
2016-02-17 15:41:10 +0800

    Initial commit of FileTypeFilter.java

---
+++ src/ui/components/FileTypeFilter.java
@@ -0,0 +1,34 @@
+/*
+ * From codejava.net: http://www.codejava.net/java-se/swing/file-picker-component-in-swing
+ */
+
+package ui.components;
+
+import java.io.File;
+import java.io.File;
+
+import javax.swing.filechooser.FileFilter;
+import javax.swing.filechooser.FileFilter;
+ 
+public class FileTypeFilter extends FileFilter {
+ 
+    private String extension;
+    private String description;
+     
+    public FileTypeFilter(String extension, String description) {
+        this.extension = extension;
+        this.description = description;
+    }
+     
+    @Override
+    public boolean accept(File file) {
+        if (file.isDirectory()) {
+            return true;
+        }
+        return file.getName().toLowerCase().endsWith(extension);
+    }
+     
+    public String getDescription() {
+        return description + String.format(" (*%s)", extension);
+    }
+}
