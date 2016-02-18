62a582db96ca4808c4ec860ec902f6ebf26ead81
markteo <mark-543210@hotmail.com>
2016-02-18 11:27:27 +0800

    move HttpDownloadUtility file

---
+++ src/httpUtil/task/HttpDownloadUtility.java
@@ -0,0 +1,114 @@
+package httpUtil.task;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import javax.swing.JOptionPane;
+import javax.swing.SwingWorker;
+
+import ui.UIFileDownloadHTTP;
+
+/**
+ * A utility that downloads a file from a URL.
+ * @author www.codejava.net
+ *
+ */
+public class HttpDownloadUtility extends SwingWorker<Void, Void>{
+	private static final int BUFFER_SIZE = 4096;
+	private static UIFileDownloadHTTP gui;
+
+	/**
+	 * Downloads a file from a URL
+	 * @param fileURL HTTP URL of the file to be downloaded
+	 * @param saveDir path of the directory to save the file
+	 * @throws IOException
+	 */
+	public String downloadFile(String fileURL, String saveDir, String sessionKey, ui.UIFileDownloadHTTP ui)
+			{
+		try{
+		gui = ui;
+		URL url = new URL(fileURL + "?session-key=" + sessionKey);
+		HttpURLConnection httpConn = (HttpURLConnection) url.openConnection();
+		httpConn.setRequestMethod("GET");
+		int responseCode = httpConn.getResponseCode();
+
+		// always check HTTP response code first
+		if (responseCode == HttpURLConnection.HTTP_OK) {
+			String fileName = "";
+			String disposition = httpConn.getHeaderField("Content-Disposition");
+			String contentType = httpConn.getContentType();
+			int contentLength = httpConn.getContentLength();
+
+			if (disposition != null) {
+				// extracts file name from header field
+				int index = disposition.indexOf("filename=");
+				if (index > 0) {
+					fileName = disposition.substring(index + 10,
+							disposition.length() - 1);
+				}
+			} else {
+				// extracts file name from URL
+				fileName = fileURL.substring(fileURL.lastIndexOf("/") + 1,
+						fileURL.length());
+			}
+
+			System.out.println("Content-Type = " + contentType);
+			System.out.println("Content-Disposition = " + disposition);
+			System.out.println("Content-Length = " + contentLength);
+			System.out.println("fileName = " + fileName);
+			
+			// opens input stream from the HTTP connection
+			gui.setFileInfo(fileName,contentLength);
+
+			InputStream inputStream = httpConn.getInputStream();
+			String saveFilePath = saveDir + File.separator + fileName;
+			
+			// opens an output stream to save into file
+			FileOutputStream outputStream = new FileOutputStream(saveFilePath);
+
+			long totalBytesRead = 0;
+			int percentCompleted = 0;
+			long fileSize = contentLength;
+			
+			int bytesRead = -1;
+			byte[] buffer = new byte[BUFFER_SIZE];
+			while ((bytesRead = inputStream.read(buffer)) != -1) {
+				outputStream.write(buffer, 0, bytesRead);
+				totalBytesRead += bytesRead;
+				percentCompleted = (int) (totalBytesRead * 100 / fileSize);
+
+				setProgress(percentCompleted);
+			}
+
+			outputStream.close();
+			inputStream.close();
+
+			System.out.println("File downloaded");
+			httpConn.disconnect();
+			return "OK";
+		} else {
+			System.out.println("No file to download. Server replied HTTP code: " + responseCode);
+			httpConn.disconnect();
+			return "ERROR";
+			}
+		}catch(IOException e){
+			JOptionPane.showMessageDialog(gui, "Error downloading file: " + e.getMessage(), "Error",
+					JOptionPane.ERROR_MESSAGE);
+			e.printStackTrace();
+			setProgress(0);
+			cancel(true);
+		}
+		
+		return null;
+	}
+
+	@Override
+	protected Void doInBackground() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
