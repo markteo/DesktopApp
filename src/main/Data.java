5b7860b68955831ef053e36dc0b9dda9981f196c
markteo <mark-543210@hotmail.com>
2016-02-23 17:10:57 +0800

    added more data to be stored

--- src/main/Data.java
+++ src/main/Data.java
@@ -1,5 +1,8 @@
 package main;
 
+import ui.UIBucketSelect;
+import ui.UIInventorySelect;
+
 public class Data {
 	public static String bucket;
 	public static String targetURL;
@@ -7,6 +10,9 @@ public class Data {
 	public static boolean accessRights = false;
 	public static String URL;
 	public static String registrationNumber;
+	public static int bucketID;
+	public static UIInventorySelect uiInventorySelect;
+	public static UIBucketSelect uiBucketSelect;
 	
 	
 }
