f22090a56d6eb14c0b68e4d2b7161c07f25840d1
markteo <mark-543210@hotmail.com>
2016-02-24 11:36:03 +0800

    integrate UILicenseSelect

--- src/ui/UILicenseSelect.java
+++ src/ui/UILicenseSelect.java
@@ -34,13 +34,13 @@ import customColor.CustomColor;
 
 public class UILicenseSelect {
 	public static APICall api = new APICall();
-
 	public void runLicenseSelect() {
 		Panel p = new Panel();
 		Button b = new Button();
 		Label l = new Label();
 
-		DefaultListModel<String> model = new DefaultListModel<String>();
+		DefaultListModel<String> model;
+		model = getLicenseData(new DefaultListModel<String>());
 		// fetch list from server
 		
 //		JSONArray LicenseList = new APIProcess().LicenseList(Data.targetURL, Data.sessionKey);
@@ -94,11 +94,9 @@ public class UILicenseSelect {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				// do something with selected License
-				System.out.println(listLicense.getModel().getElementAt(listLicense.getSelectedIndex()));
 				String itemSelected = listLicense.getModel().getElementAt(listLicense.getSelectedIndex()).toString();
-				String[] itemData = itemSelected.split("\\,");
-				Data.registrationNumber = itemData[1].trim();
-				System.out.println(Data.registrationNumber);
+				Data.licenseNumber = itemSelected;
+				System.out.println(Data.licenseNumber);
 			}
 		});
 
@@ -109,6 +107,21 @@ public class UILicenseSelect {
 		LicenseFrame.add(pnlLicenseList, BorderLayout.CENTER);
 		LicenseFrame.add(pnlButtons, BorderLayout.SOUTH);
 		LicenseFrame.pack();
+	}
+	private static DefaultListModel<String> getLicenseData(DefaultListModel<String> model){
+		
+		JSONArray licenseList = new APIProcess().nodeLicenseList(Data.targetURL, Data.sessionKey, Data.bucketID);
+
+		try {
+			for (int i = 0; i < licenseList.length(); i++) {
+				JSONObject license = licenseList.getJSONObject(i);
+				model.addElement(license.getString("licenseNumber"));
+			}
+		} catch (JSONException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		return model;
 	}
 
 }
