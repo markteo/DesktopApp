b907f3c35f58f08bcf49391f83a63883d701c8ec
markteo <mark-543210@hotmail.com>
2016-03-11 16:19:02 +0800

    implement threading and loading login screen

--- src/main/DesktopAppMain.java
+++ src/main/DesktopAppMain.java
@@ -1,20 +1,53 @@
 package main;
-import org.json.JSONArray;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dialog.ModalityType;
+import java.awt.Desktop;
+import java.awt.Dimension;
+import java.awt.Graphics2D;
+import java.awt.SplashScreen;
+import java.awt.Window;
+import java.awt.event.ActionEvent;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
+import javax.swing.AbstractAction;
+import javax.swing.AbstractButton;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JProgressBar;
+import javax.swing.SwingUtilities;
+import javax.swing.SwingWorker;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import ui.UILogin;
-import api.APICall;
 
-public class DesktopAppMain {
+public class DesktopAppMain{
 	
+	public static JFrame frame;
 
 	public static void main(String args[]) {
+		frame = new JFrame("KAI Tool QR Generator");
+		JButton showWaitBtn = new JButton(
+				new ShowWaitAction("Start"));
+	    frame.add(showWaitBtn, BorderLayout.CENTER);
+	    frame.setSize(300, 95);
+	    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    frame.setVisible(true);
+
+//		Data.loadingFrame.setVisible(true);
+//		Data.loadingScreen = new JDialog(Data.loadingFrame, "Busy", ModalityType.DOCUMENT_MODAL);
+//		Data.loadingScreen.setSize(200, 150);
+//		Data.loadingScreen.setLocationRelativeTo(Data.loadingFrame);
 		
-		Data.uiLogin = new UILogin();
-		Data.uiLogin.runLogin();
-		
+
 	}
+	
 	public static boolean checkResult (String response){
 		boolean result = false;
 		
@@ -30,5 +63,56 @@ public class DesktopAppMain {
 		}
 		
 		return result;
+	}
+	
+	
+}
+class ShowWaitAction extends AbstractAction {
+
+	public ShowWaitAction(String name) {
+		super(name);
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent evt) {
+		SwingWorker<Void, Void> mySwingWorker = new SwingWorker<Void, Void>() {
+			@Override
+			protected Void doInBackground() throws Exception {
+
+				Data.uiLogin = new UILogin();
+				Data.uiLogin.runLogin();
+				DesktopAppMain.frame.setVisible(false);
+				
+				return null;
+			}
+		};
+
+		Window win = SwingUtilities.getWindowAncestor((AbstractButton) evt
+				.getSource());
+		final JDialog dialog = new JDialog(win, "Loading",
+				ModalityType.APPLICATION_MODAL);
+
+		mySwingWorker.addPropertyChangeListener(new PropertyChangeListener() {
+
+			@Override
+			public void propertyChange(PropertyChangeEvent evt) {
+				if (evt.getPropertyName().equals("state")) {
+					if (evt.getNewValue() == SwingWorker.StateValue.DONE) {
+						dialog.dispose();
+					}
+				}
+			}
+		});
+		mySwingWorker.execute();
+
+		JProgressBar progressBar = new JProgressBar();
+		progressBar.setIndeterminate(true);
+		JPanel panel = new JPanel(new BorderLayout());
+		panel.add(progressBar, BorderLayout.CENTER);
+		panel.add(new JLabel("Initializing......."), BorderLayout.PAGE_START);
+		dialog.add(panel);
+		dialog.pack();
+		dialog.setLocationRelativeTo(win);
+		dialog.setVisible(true);
 	}
 }
