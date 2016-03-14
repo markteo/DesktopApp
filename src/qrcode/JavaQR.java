f3753fcdd714126256d32f5da55406f357d7399f
markteo <mark-543210@hotmail.com>
2016-03-14 17:29:08 +0800

    Refactor UI

--- src/qrcode/JavaQR.java
+++ src/qrcode/JavaQR.java
@@ -2,14 +2,20 @@
 package qrcode;
 
 import java.awt.BorderLayout;
-import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
+import javax.imageio.ImageIO;
 import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFormattedTextField;
@@ -44,11 +50,11 @@ public class JavaQR implements Runnable {
 	public void run() {
 		//		Initial Config
 		frame = new JFrame("QR Code Generator");
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	
 		frame.setLayout(new BorderLayout());
 
 		JPanel topPanel = new JPanel();
 		JPanel centerPanel = new JPanel();
+		JPanel bottomPanel = new JPanel();
 		
 		//		Top Panel
 		topPanel.setLayout(new GridLayout(0,1));
@@ -91,58 +97,98 @@ public class JavaQR implements Runnable {
 		rowTopPanel.add(regField);
 		rowTopPanel.add(licNo);
 		rowTopPanel.add(licField);
-		rowTopPanel.add(downloadLoc);
-		rowTopPanel.add(dlBtn);
+		
 		topPanel.add(rowTopPanel);
 
 		//		Center Panel
 		centerPanel.setLayout(new GridLayout(0,1));
-		centerPanel.setBorder(BorderFactory.createTitledBorder("Generate QR"));
-		centerPanel.setPreferredSize(new Dimension(100,60));
+		centerPanel.setBorder(BorderFactory.createTitledBorder("QR Code"));
+//		centerPanel.setPreferredSize(new Dimension(100,60));
 
 		JPanel rowCenPanel = new JPanel();
 		rowCenPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
 
-		JButton genBtn = new JButton("Generate QR");
+		JButton genBtn = new JButton("Download QR Code");
 		JButton homeBtn = new JButton("Back to Start");
 		JButton logoutBtn = new JButton("Logout");
 		
+		String accessKey = accField.getText().toString();
+		String regKey = regField.getText().toString();
+		String licKey = licField.getText().toString();
+		String dlDir = dlBtn.getText().toString();
+		JSONObject jsonObject = new JSONObject();
+		try {
+			jsonObject.put("accessKey", accessKey);
+			jsonObject.put("registrationNumber", regKey);
+			jsonObject.put("licenseNumber", licKey);
+		} catch (JSONException e1) {
+			e1.printStackTrace();
+		}
 
+		String fileLocation = dlBtn.getText().toString();
+		fileLocation = "./";
+		QRLogic qrGen = new QRLogic();
+		BufferedImage image = qrGen.generateQR(jsonObject);
+		centerPanel.add(new JLabel(new ImageIcon(image)));
+		
+		bottomPanel.setLayout(new GridLayout(2, 1));
+		JPanel rowBottom1 = new JPanel();
+		rowBottom1.setLayout(new FlowLayout(FlowLayout.CENTER));
+
+		rowBottom1.add(downloadLoc);
+		rowBottom1.add(dlBtn);
 		rowCenPanel.add(genBtn);
 		rowCenPanel.add(homeBtn);
 		rowCenPanel.add(logoutBtn);
-		centerPanel.add(rowCenPanel);
+		bottomPanel.add(rowBottom1);
+		bottomPanel.add(rowCenPanel);
+		frame.add(topPanel, BorderLayout.NORTH);
+		frame.add(bottomPanel, BorderLayout.SOUTH);
+		frame.add(centerPanel, BorderLayout.CENTER);
+		frame.setLocationRelativeTo(null);
+		frame.setSize(1000, 500);
+		frame.pack();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	
+		frame.setVisible(true);
+		
+		
+
+//		if(accessKey.trim().equalsIgnoreCase("") == true || regKey.trim().equalsIgnoreCase("") == true) {
+//			JOptionPane.showMessageDialog(frame, "Some fields are missing!");
+//		} else if(licKey.trim().length() < 21) {
+//			JOptionPane.showMessageDialog(frame, "License Key is incomplete!");
+//		} else if(dlDir.equalsIgnoreCase("Choose Download Folder") != true ) {
+			
+			//JOptionPane.showMessageDialog(frame, "QR Code Generated in " + fileLocation);
+//		} else {
+//			JOptionPane.showMessageDialog(frame, "Choose a directory!");
+//			System.out.println("No QR Generated");
+//		}
 
 //		Buttons Listener
 		genBtn.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-
-				String accessKey = accField.getText().toString();
-				String regKey = regField.getText().toString();
-				String licKey = licField.getText().toString();
+				Date date = new Date();
+				String newDate = new SimpleDateFormat("yyyy-MM-dd h-m-a").format(date);
 				String dlDir = dlBtn.getText().toString();
 
-				if(accessKey.trim().equalsIgnoreCase("") == true || regKey.trim().equalsIgnoreCase("") == true) {
-					JOptionPane.showMessageDialog(frame, "Some fields are missing!");
-				} else if(licKey.trim().length() < 21) {
-					JOptionPane.showMessageDialog(frame, "License Key is incomplete!");
-				} else if(dlDir.equalsIgnoreCase("Choose Download Folder") != true ) {
-					JSONObject jsonObject = new JSONObject();
-					try {
-						jsonObject.put("accessKey", accessKey);
-						jsonObject.put("registrationNumber", regKey);
-						jsonObject.put("licenseNumber", licKey);
-					} catch (JSONException e1) {
-						e1.printStackTrace();
-					}
+				String filePath = dlDir + "/qrcode-" + newDate + ".png";
+				String fileType = "png";
+
+				if(dlDir.equalsIgnoreCase("Choose Download Folder") != true ) {
 
 					String fileLocation = dlBtn.getText().toString();
-
-					QRLogic qrGen = new QRLogic();
-					qrGen.generateQR(jsonObject, fileLocation);
-					JOptionPane.showMessageDialog(frame, "QR Code Generated in " + fileLocation);
+					File myFile = new File(filePath);
+					try {
+						ImageIO.write(image, fileType, myFile);
+						JOptionPane.showMessageDialog(frame, "QR Code Saved in " + fileLocation);
+					} catch (IOException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}					
+					
 				} else {
 					JOptionPane.showMessageDialog(frame, "Choose a directory!");
 					System.out.println("No QR Generated");
@@ -209,13 +255,7 @@ public class JavaQR implements Runnable {
 			e1.printStackTrace();
 		}
 		
-		frame.add(topPanel, BorderLayout.NORTH);
-		frame.add(centerPanel, BorderLayout.CENTER);
-		frame.setLocationRelativeTo(null);
-		frame.setResizable(false);
-		frame.setSize(400,225);
-		frame.setVisible(true);
-
+		
 	}
 	
 	public void start() {
