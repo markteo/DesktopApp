37a6ed70eaeec402e9d47dab14cc215a2317f373
jccheok <cheok.jia.chin@gmail.com>
2016-03-18 11:52:46 +0800

    Add new function to align label in Label

--- src/ui/components/Label.java
+++ src/ui/components/Label.java
@@ -1,6 +1,7 @@
 package ui.components;
 
 import java.awt.Color;
+import java.awt.Dimension;
 
 import javax.swing.JLabel;
 import javax.swing.border.EmptyBorder;
@@ -8,21 +9,29 @@ import javax.swing.border.EmptyBorder;
 import customColor.CustomColor;
 
 public class Label {
-	public JLabel createLabel(String labelString){
+	public JLabel createLabel(String labelString) {
 		JLabel label = new JLabel(labelString);
 		label.setForeground(CustomColor.Grey.returnColor());
-		
+
 		return label;
 	}
-	
-	public JLabel createResultLabel(String labelString){
+
+	public JLabel createResultLabel(String labelString) {
 		JLabel label = new JLabel(labelString);
 		label.setForeground(Color.ORANGE);
-		
+
 		return label;
 	}
-	
-	public void addPadding(JLabel l, int padding){
-		l.setBorder(new EmptyBorder(padding,padding,padding,padding));
+
+	public JLabel createLabel(String labelString, int alignment) {
+		JLabel label = new JLabel(labelString, alignment);
+		label.setPreferredSize(new Dimension(150, 25));
+		label.setForeground(CustomColor.Grey.returnColor());
+
+		return label;
+	}
+
+	public void addPadding(JLabel l, int padding) {
+		l.setBorder(new EmptyBorder(padding, padding, padding, padding));
 	}
 }
