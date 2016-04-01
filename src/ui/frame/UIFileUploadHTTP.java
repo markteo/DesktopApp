dc1182bb61155624019598ea0da7bc725ac16fd8
markteo <mark-543210@hotmail.com>
2016-04-01 12:56:52 +0800

    refactor uploadUI

--- src/ui/frame/UIFileUploadHTTP.java
+++ src/ui/frame/UIFileUploadHTTP.java
@@ -6,19 +6,28 @@ import java.awt.FileDialog;
 import java.awt.FlowLayout;
 import java.awt.GridBagConstraints;
 import java.awt.Insets;
+import java.awt.Window;
+import java.awt.Dialog.ModalityType;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 import java.io.File;
 import java.io.FilenameFilter;
 import java.io.IOException;
 
+import javax.swing.AbstractButton;
 import javax.swing.JButton;
+import javax.swing.JDialog;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JProgressBar;
 import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
+import javax.swing.SwingWorker;
 
 import main.Data;
 
@@ -181,7 +190,6 @@ public class UIFileUploadHTTP extends JFrame {
 	private JButton btnBrowse;
 	private JFileChooser fc = new JFileChooser();
 
-
 	private FileDialog fd;
 
 	Panel p = new Panel();
@@ -189,7 +197,7 @@ public class UIFileUploadHTTP extends JFrame {
 	Label l = new Label();
 
 	public UIFileUploadHTTP() {
-		super("Download Inventory Template");
+		super("Upload Inventory");
 		setBackground(CustomColor.NavyBlue.returnColor());
 
 		setLayout(new BorderLayout());
@@ -211,50 +219,134 @@ public class UIFileUploadHTTP extends JFrame {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				// TODO Auto-generated method stub
 
 				APICall api = new APICall();
 				String saveDir = runFileChooser();
-				try {
-					api.getCSVSample(Data.URL, Data.sessionKey, saveDir);
-				} catch (IOException e1) {
-					e1.printStackTrace();
-				}
+				SwingWorker<Void, Void> mySwingWorker = new SwingWorker<Void, Void>() {
+					@Override
+					protected Void doInBackground() throws Exception {
+						try {
+							api.getCSVSample(Data.URL, Data.sessionKey, saveDir);
+						} catch (IOException e1) {
+							e1.printStackTrace();
+						}
+						return null;
+					}
+				};
+				
+				Window win = SwingUtilities
+						.getWindowAncestor((AbstractButton) e.getSource());
+				final JDialog dialog = new JDialog(win, "Loading",
+						ModalityType.APPLICATION_MODAL);
+
+				mySwingWorker
+						.addPropertyChangeListener(new PropertyChangeListener() {
+
+							@Override
+							public void propertyChange(PropertyChangeEvent evt) {
+								if (evt.getPropertyName().equals("state")) {
+									if (evt.getNewValue() == SwingWorker.StateValue.DONE) {
+										dialog.dispose();
+									}
+								}
+							}
+						});
+				mySwingWorker.execute();
+
+				JProgressBar progressBar = new JProgressBar();
+				progressBar.setIndeterminate(true);
+				JPanel panel = new JPanel(new BorderLayout());
+				panel.add(progressBar, BorderLayout.CENTER);
+				panel.add(new JLabel("Downloading......."),
+						BorderLayout.PAGE_START);
+				dialog.add(panel);
+				dialog.pack();
+				dialog.setBounds(50, 50, 300, 100);
+				dialog.setLocationRelativeTo(Data.mainFrame.uiFileUpload);
+				dialog.setVisible(true);
+
 			}
 		});
 		btnUpload = b.createButton("Upload");
 		btnUpload.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				// insert api for upload
-				String fileURL = tfFileName.getText();
+				SwingWorker<Void, Void> mySwingWorker = new SwingWorker<Void, Void>() {
+					@Override
+					protected Void doInBackground() throws Exception {
+						// insert api for upload
+						String fileURL = tfFileName.getText();
 
-				if (fileURL.isEmpty()) {
-					final JPanel panel = new JPanel();
-					JOptionPane.showMessageDialog(panel, "No file selected", "Error", JOptionPane.ERROR_MESSAGE);
-					return;
-				} else {
-					APICall api = new APICall();
-					String response = api.uploadInventory(Data.targetURL, fileURL, Data.sessionKey);
-					try {
-						JSONObject responseObject = new JSONObject(response);
-						if (responseObject.get("result").equals("ok")) {
-							JOptionPane.showMessageDialog(panel, "File uploaded", "Success",
-									JOptionPane.INFORMATION_MESSAGE);
-							Data.mainFrame.uiInventorySelect.updateInventoryList();
-						} else {
-							JOptionPane.showMessageDialog(Data.mainFrame, "Please check the file", "Error",
+						if (fileURL.isEmpty()) {
+							final JPanel panel = new JPanel();
+							JOptionPane.showMessageDialog(panel,
+									"No file selected", "Error",
 									JOptionPane.ERROR_MESSAGE);
-						}
+							return null;
+						} else {
+							APICall api = new APICall();
+							String response = api.uploadInventory(
+									Data.targetURL, fileURL, Data.sessionKey);
+							try {
+								JSONObject responseObject = new JSONObject(
+										response);
+								if (responseObject.get("result").equals("ok")) {
+									JOptionPane.showMessageDialog(panel,
+											"File uploaded", "Success",
+											JOptionPane.INFORMATION_MESSAGE);
+									Data.mainFrame.uiInventorySelect
+											.updateInventoryList();
+									Data.mainFrame.uiFileUpload.setVisible(false);
+								} else {
+									JOptionPane.showMessageDialog(
+											Data.mainFrame,
+											"Please check the file", "Error",
+											JOptionPane.ERROR_MESSAGE);
+								}
 
-					} catch (JSONException e1) {
-						// TODO Auto-generated catch block
-						e1.printStackTrace();
+							} catch (JSONException e1) {
+								// TODO Auto-generated catch block
+								e1.printStackTrace();
+							}
+						}
+						return null;
 					}
-				}
+
+				};
+
+				Window win = SwingUtilities
+						.getWindowAncestor((AbstractButton) e.getSource());
+				final JDialog dialog = new JDialog(win, "Loading",
+						ModalityType.APPLICATION_MODAL);
+
+				mySwingWorker
+						.addPropertyChangeListener(new PropertyChangeListener() {
+
+							@Override
+							public void propertyChange(PropertyChangeEvent evt) {
+								if (evt.getPropertyName().equals("state")) {
+									if (evt.getNewValue() == SwingWorker.StateValue.DONE) {
+										dialog.dispose();
+									}
+								}
+							}
+						});
+				mySwingWorker.execute();
+
+				JProgressBar progressBar = new JProgressBar();
+				progressBar.setIndeterminate(true);
+				JPanel panel = new JPanel(new BorderLayout());
+				panel.add(progressBar, BorderLayout.CENTER);
+				panel.add(new JLabel("Uploading......."),
+						BorderLayout.PAGE_START);
+				dialog.add(panel);
+				dialog.pack();
+				dialog.setBounds(50, 50, 300, 100);
+				dialog.setLocationRelativeTo(Data.mainFrame.uiFileUpload);
+				dialog.setVisible(true);
 			}
 		});
-		btnBack = b.createButton("Back");
+		btnBack = b.createButton("Cancel");
 		btnBack.addActionListener(new ActionListener() {
 
 			@Override
@@ -305,7 +397,8 @@ public class UIFileUploadHTTP extends JFrame {
 		l.addPadding(lblFileName, 10);
 		tfFileName = new JTextField();
 		tfFileName.setEditable(false);
-		tfFileName.setPreferredSize(new Dimension(225, tfFileName.getPreferredSize().height));
+		tfFileName.setPreferredSize(new Dimension(225, tfFileName
+				.getPreferredSize().height));
 
 		g.gridx = 0;
 		g.gridy = 0;
@@ -323,6 +416,7 @@ public class UIFileUploadHTTP extends JFrame {
 
 		return panel;
 	}
+
 	public String runFileChooser() {
 		fc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
 		fc.showSaveDialog(null);
