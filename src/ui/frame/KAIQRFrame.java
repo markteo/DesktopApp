c2d46fd11bc41b60fe60b3fdcf78033b8ec9e63e
jccheok <cheok.jia.chin@gmail.com>
2016-04-08 11:11:40 +0800

    Added Logout button, removed Window Frame for KAIQRFrame

--- src/ui/frame/KAIQRFrame.java
+++ src/ui/frame/KAIQRFrame.java
@@ -3,8 +3,15 @@ package ui.frame;
 import java.awt.BorderLayout;
 import java.awt.CardLayout;
 import java.awt.Dimension;
+import java.awt.Point;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
@@ -34,31 +41,71 @@ public class KAIQRFrame extends JFrame {
 	public JPanel cardPanel;
 	public CardLayout cardLayout;
 
+	private JPanel menuPanel;
+	private JButton btnLogOut;
+
 	Panel p = new Panel();
 	Button b = new Button();
 	Label l = new Label();
 
+	static Point mouseDownCompCoords;
+
 	public KAIQRFrame() {
 		// init frames
 		super("KAI QR Code Generator");
+		mouseDownCompCoords = null;
+		addMouseListener(new MouseListener() {
+			public void mouseReleased(MouseEvent e) {
+				mouseDownCompCoords = null;
+			}
+
+			public void mousePressed(MouseEvent e) {
+				mouseDownCompCoords = e.getPoint();
+			}
+
+			public void mouseExited(MouseEvent e) {
+			}
+
+			public void mouseEntered(MouseEvent e) {
+			}
+
+			public void mouseClicked(MouseEvent e) {
+			}
+		});
+
+		addMouseMotionListener(new MouseMotionListener() {
+			public void mouseMoved(MouseEvent e) {
+			}
+
+			public void mouseDragged(MouseEvent e) {
+				Point currCoords = e.getLocationOnScreen();
+				setLocation(currCoords.x - mouseDownCompCoords.x, currCoords.y - mouseDownCompCoords.y);
+			}
+		});
+
+		setUndecorated(true);
 		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
+
+		menuPanel = p.createPanel(Layouts.grid, 5, 1);
+		btnLogOut = b.createButton("Logout");
+		btnLogOut.setPreferredSize(new Dimension(100, 50));
+		btnLogOut.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				System.exit(DISPOSE_ON_CLOSE);
+			}
+		});
+		menuPanel.add(btnLogOut);
 
 		cardPanel = p.createPanel(Layouts.card);
 		cardLayout = new CardLayout();
 		cardPanel.setLayout(cardLayout);
-		//
-		// cardPanel.add(uiInventorySelect, "inventory");
-		// cardPanel.add(uiFileUpload, "upload");
-		// cardPanel.add(uiBucketSelect, "bucket");
-		// cardPanel.add(uiLicenseDetail, "license");
-		// cardPanel.add(uiAccessKeySelect, "access");
-		// cardPanel.add(uiGenerateKey, "generate");
-		// cardPanel.add(qrGenerator, "qr");
 
 		getContentPane().setBackground(CustomColor.NavyBlue.returnColor());
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setLayout(new BorderLayout());
 		setLocationRelativeTo(null);
+		add(menuPanel, BorderLayout.WEST);
 		add(cardPanel, BorderLayout.CENTER);
 		pack();
 	}
